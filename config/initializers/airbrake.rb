865043a368b8684406a3666e1d9631183861aaeb
Mikhael Rakauskas <mrakauskas@careerplug.com>
2025-08-15 15:39:12 -0400

    Rubocop tweak

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 unless ENV['DOCKER_BUILD']
   Airbrake.configure do |config|
-    config.project_key = ENV['AIRBRAKE_KEY']
-    config.project_id = ENV['AIRBRAKE_ID']
+    config.project_key = ENV.fetch('AIRBRAKE_KEY')
+    config.project_id = ENV.fetch('AIRBRAKE_ID')
     config.environment = Rails.env
     config.ignore_environments = %w[development test]
     config.blocklist_keys = Rails.application.config.filter_parameters
