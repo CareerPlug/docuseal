55616b474a8a519cea207a44d21ece6e88af1c98
Mikhael Rakauskas <mrakauskas@careerplug.com>
2025-08-25 15:05:58 -0400

    Fix bang on prod script, other script tweaks, and relic/airbrake config issues

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -6,5 +6,6 @@ unless ENV['DOCKER_BUILD'] || ENV['CI_BUILD']
     config.project_id = ENV['AIRBRAKE_ID'] # rubocop:disable Style/FetchEnvVar
     config.environment = Rails.env
     config.ignore_environments = %w[development test]
+    config.root_directory = '/var/cpd/app'
   end
 end
