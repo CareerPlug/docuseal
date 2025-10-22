31c867cffd05476506d81420c2b5d11d4184ad93
Ryan Arakawa <arakawa.ryan@gmail.com>
2025-10-21 21:03:59 -0500

    add partnership context and tests to submissions

--- lib/abilities/submission_conditions.rb
+++ lib/abilities/submission_conditions.rb
@@ -7,13 +7,13 @@ module Abilities
   module SubmissionConditions
     module_function
 
-    def collection(user)
+    def collection(user, request_context: nil)
       return [] if user.account_id.blank?
 
-      submissions_for_user(user)
+      submissions_for_user(user, request_context)
     end
 
-    def entity(submission, user:)
+    def entity(submission, user:, request_context: nil)
       # Only account users can access submissions
       return false if user.account_id.blank?
 
@@ -24,13 +24,13 @@ module Abilities
         template = submission.template || Template.find_by(id: submission.template_id)
         return false unless template
 
-        return true if user_can_access_template?(user, template)
+        return true if user_can_access_template?(user, template, request_context)
       end
       false
     end
 
-    def submissions_for_user(user)
-      accessible_template_ids = accessible_template_ids(user)
+    def submissions_for_user(user, request_context = nil)
+      accessible_template_ids = accessible_template_ids(request_context)
 
       Submission.where(
         'submissions.account_id = ? OR submissions.template_id IN (?)',
@@ -39,12 +39,14 @@ module Abilities
       )
     end
 
-    def accessible_template_ids(user)
+    def accessible_template_ids(request_context = nil)
       template_ids = []
 
-      # Add templates from user's partnership (inheritance)
-      if user.account.partnership_id.present?
-        template_ids += Template.where(partnership_id: user.account.partnership_id).pluck(:id)
+      # Add templates from partnership context (if provided via API)
+      if request_context&.dig(:accessible_partnership_ids).present?
+        accessible_partnership_ids = request_context[:accessible_partnership_ids]
+        partnership_ids = Partnership.where(external_partnership_id: accessible_partnership_ids).pluck(:id)
+        template_ids += Template.where(partnership_id: partnership_ids).pluck(:id)
       end
 
       # Add templates from global partnership (accessible to everyone)
@@ -55,16 +57,27 @@ module Abilities
       template_ids.uniq
     end
 
-    def user_can_access_template?(user, template)
-      return true if inherited_partnership_template?(user, template)
+    def user_can_access_template?(user, template, request_context = nil)
+      # User can access templates from their account
+      return true if template.account_id == user.account_id
+
+      # Check partnership context access
+      return true if partnership_context_accessible?(template, request_context)
+
+      # Check global partnership access
       return true if global_template_accessible?(template)
 
       false
     end
 
-    def inherited_partnership_template?(user, template)
-      user.account.partnership_id.present? &&
-        template.partnership_id == user.account.partnership_id
+    def partnership_context_accessible?(template, request_context)
+      return false if request_context&.dig(:accessible_partnership_ids).blank?
+      return false if template.partnership_id.blank?
+
+      accessible_partnership_ids = request_context[:accessible_partnership_ids]
+      accessible_partnerships = Partnership.where(external_partnership_id: accessible_partnership_ids)
+
+      accessible_partnerships.exists?(id: template.partnership_id)
     end
 
     def global_template_accessible?(template)
