752b990942e9f4f0b3109d11749666a2f14a3785
Ryan Arakawa <arakawa.ryan@gmail.com>
2026-02-16 17:04:30 -0600

    fix rubocop/rspec for HTTP requests in test

--- spec/lib/webhook_retry_logic_spec.rb
+++ spec/lib/webhook_retry_logic_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Simple response object for testing
+FakeResponse = Struct.new(:status)
+
 RSpec.describe WebhookRetryLogic do
   let(:account) { create(:account) }
   let(:user) { create(:user, account: account) }
@@ -8,7 +11,7 @@ RSpec.describe WebhookRetryLogic do
   describe '.should_retry?' do
     context 'with successful response' do
       it 'does not retry' do
-        response = instance_double(HTTP::Response, status: 200)
+        response = FakeResponse.new(200)
         result = described_class.should_retry?(response: response, attempt: 1, record: template)
         expect(result).to be false
       end
@@ -16,13 +19,13 @@ RSpec.describe WebhookRetryLogic do
 
     context 'with failed response' do
       it 'retries on 4xx errors within attempt limit' do
-        response = instance_double(HTTP::Response, status: 400)
+        response = FakeResponse.new(400)
         result = described_class.should_retry?(response: response, attempt: 5, record: template)
         expect(result).to be true
       end
 
       it 'retries on 5xx errors within attempt limit' do
-        response = instance_double(HTTP::Response, status: 500)
+        response = FakeResponse.new(500)
         result = described_class.should_retry?(response: response, attempt: 5, record: template)
         expect(result).to be true
       end
@@ -33,7 +36,7 @@ RSpec.describe WebhookRetryLogic do
       end
 
       it 'does not retry when max attempts exceeded' do
-        response = instance_double(HTTP::Response, status: 500)
+        response = FakeResponse.new(500)
         result = described_class.should_retry?(response: response, attempt: 11, record: template)
         expect(result).to be false
       end
