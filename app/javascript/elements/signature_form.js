9522304c5e194d5712c96e0b9cde6182f9411e36
Bernardo Anderson <bern.anderson@proton.me>
2026-09-14 20:28:22 -0500

    CP-14535 - Migrate custom-element signature pads to shared canvas sizing

--- app/javascript/elements/signature_form.js
+++ app/javascript/elements/signature_form.js
@@ -1,5 +1,6 @@
 import { target, targetable } from '@github/catalyst/lib/targetable'
 import { cropCanvasAndExportToPNG } from '../submission_form/crop_canvas'
+import { setupCanvasSizing } from '../submission_form/canvas_sizing'
 
 export default targetable(class extends HTMLElement {
   static [target.static] = ['canvas', 'input', 'clear', 'button']
@@ -25,103 +26,20 @@ export default targetable(class extends HTMLElement {
       this.submit()
     })
 
-    this.setupCanvasSizing()
+    this.canvasSizing = setupCanvasSizing({
+      getCanvas: () => this.canvas,
+      getPad: () => this.pad,
+      scale: this.scale,
+      nextSize: (canvas) => ({
+        width: canvas.parentNode.clientWidth * this.scale,
+        height: canvas.parentNode.clientHeight * this.scale
+      }),
+      perAxisStrokeRatios: true
+    })
   }
 
   disconnectedCallback () {
-    this.teardownCanvasSizing()
-  }
-
-  setupCanvasSizing () {
-    this.resizeCanvas()
-
-    this.onResizeCanvas = () => {
-      if (this.resizeRaf) {
-        cancelAnimationFrame(this.resizeRaf)
-      }
-
-      this.resizeRaf = requestAnimationFrame(() => {
-        this.resizeRaf = requestAnimationFrame(() => {
-          this.resizeCanvas()
-        })
-      })
-    }
-
-    window.addEventListener('resize', this.onResizeCanvas)
-    screen?.orientation?.addEventListener('change', this.onResizeCanvas)
-
-    if (typeof ResizeObserver !== 'undefined' && this.canvas?.parentNode) {
-      this.resizeObserver = new ResizeObserver(this.onResizeCanvas)
-      this.resizeObserver.observe(this.canvas.parentNode)
-    }
-  }
-
-  teardownCanvasSizing () {
-    if (this.resizeRaf) {
-      cancelAnimationFrame(this.resizeRaf)
-      this.resizeRaf = null
-    }
-
-    if (this.onResizeCanvas) {
-      window.removeEventListener('resize', this.onResizeCanvas)
-      screen?.orientation?.removeEventListener('change', this.onResizeCanvas)
-    }
-
-    this.resizeObserver?.disconnect()
-  }
-
-  resizeCanvas () {
-    if (!this.canvas?.parentNode) {
-      return
-    }
-
-    const width = this.canvas.parentNode.clientWidth
-    const height = this.canvas.parentNode.clientHeight
-
-    if (!width || !height) {
-      return
-    }
-
-    const nextW = width * this.scale
-    const nextH = height * this.scale
-
-    if (this.canvas.width === nextW && this.canvas.height === nextH) {
-      return
-    }
-
-    const prevCssW = this.canvas.width / this.scale
-    const prevCssH = this.canvas.height / this.scale
-    const ratioX = prevCssW > 0 ? width / prevCssW : 1
-    const ratioY = prevCssH > 0 ? height / prevCssH : 1
-
-    let data = []
-
-    if (this.pad) {
-      data = this.pad.toData()
-
-      if (data.length && (ratioX !== 1 || ratioY !== 1)) {
-        data = data.map((group) => ({
-          ...group,
-          points: group.points.map((point) => ({
-            ...point,
-            x: point.x * ratioX,
-            y: point.y * ratioY
-          }))
-        }))
-      }
-    }
-
-    this.canvas.width = nextW
-    this.canvas.height = nextH
-    this.canvas.getContext('2d').scale(this.scale, this.scale)
-
-    if (this.pad) {
-      this.pad.clear()
-
-      if (data.length) {
-        this.pad.fromData(data)
-      }
-    }
+    this.canvasSizing?.teardown()
   }
 
   async submit () {
